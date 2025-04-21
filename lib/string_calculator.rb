f7074787519c3485146befc5ed1239ce18b91914
arvind4501 <arvindjangirlpu@gmail.com>
2025-04-21 23:43:33 +0530

    Handled single number input in the add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,5 +3,6 @@ require 'byebug'
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    numbers.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,9 +2,14 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
+    let(:calculator) { StringCalculator.new }
+
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
+    end
+
+    it 'returns the number for a single number' do
+      expect(calculator.add('1')).to eq(1)
     end
   end
 end
